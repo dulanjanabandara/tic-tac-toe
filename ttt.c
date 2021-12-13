73e8d02800fb807377fdb7becfe7d32ac8662f51
dulanjanabandara <dulanjanauop@gmail.com>
2021-12-13 19:12:16 +0530

    veryfySelection functionality implemented

--- ttt.c
+++ ttt.c
@@ -21,6 +21,36 @@ int main(){
     }
     displayBoard();
 
+    while ( cWhoWon == ' ' ) {
+        printf("\n%c\n", cWhoWon);
+        if ( iCurrentPlayer == 1 || iCurrentPlayer == 0) {
+            printf("\nPLAYER X\n");
+            printf("Enter an available square number (1-9): ");
+            scanf("%d", &iSquareNum);
+
+            if ( verifySelection(iSquareNum, iCurrentPlayer) == 1 ) {
+                iCurrentPlayer = 1;
+            }
+            else {
+                iCurrentPlayer = 2;
+            }
+        }
+        else {
+            printf("\nPLAYER O\n");
+            printf("Enter an available square number (1-9): ");
+            scanf("%d", &iSquareNum);
+
+            if ( verifySelection(iSquareNum, iCurrentPlayer) == 1 ) {
+                iCurrentPlayer = 2;
+            }
+            else {
+                iCurrentPlayer = 1;
+            }
+        }
+        displayBoard();
+        checkForWin();
+    }
+
     return 0;
 }
 
@@ -30,11 +60,27 @@ void displayBoard(){
     printf("\n\t|\t|\n");
     printf("\t|\t|\n");
     printf("%c\t|%c\t|%c\n", board[0], board[1], board[2]);
-    printf("----|----|----\n");
+    printf("--------|-------|--------\n");
     printf("\t|\t|\n");
     printf("%c\t|%c\t|%c\n", board[3], board[4], board[5]);
-    printf("----|----|----\n");
+    printf("--------|-------|--------\n");
     printf("\t|\t|\n");
     printf("%c\t|%c\t|%c\n", board[6], board[7], board[8]);
     printf("\t|\t|\n");
 }
+
+// verifySelection function definition
+int verifySelection(int iSquare, int iPlayer) {
+    if ( board[iSquare - 1] == ' ' && (iPlayer == 1 || iPlayer == 0) ) {
+        board[iSquare - 1] = 'X';
+        return 0;
+    }
+    else if ( board[iSquare - 1] == ' ' && iPlayer == 2 ) {
+        board[iSquare - 1] = 'O';
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
